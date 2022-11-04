d29ea50f153919ef89737b34fe31221e3f832f69
Chi Nguyen <chi.nguyen@live.com>
2022-11-04 22:35:25 +0200

    static content of public folder

--- server-one/index.js
+++ server-one/index.js
@@ -4,8 +4,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+let requestCounter = 0;
+
+app.use(express.static("public"));
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+app.get("/test", (req, res) => {
+  console.log("Testing page access");
+  requestCounter++;
+  res.send("<h1>Test page</h1><h2>" + requestCounter + "</h2>");
 });
 
 app.listen(port, () => {
