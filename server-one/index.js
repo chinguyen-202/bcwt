5fc22ee435b3cd9ac0b91bde2b10ab3468b0b530
Chi Nguyen <chi.nguyen@live.com>
2022-11-04 22:36:45 +0200

    add endpoint catinfo to index.js

--- server-one/index.js
+++ server-one/index.js
@@ -18,6 +18,15 @@ app.get("/test", (req, res) => {
   res.send("<h1>Test page</h1><h2>" + requestCounter + "</h2>");
 });
 
+app.get("/catinfo", (req, res) => {
+  const cat = {
+    name: "Frank",
+    birthdate: "2010-12-25",
+    weight: 5,
+  };
+  res.json(cat);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
