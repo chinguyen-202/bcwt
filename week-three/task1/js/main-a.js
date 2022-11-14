08e17a63046e2b7a6ca017d35321d43eaae29edd
Chi Nguyen <chi.nguyen@live.com>
2022-11-14 13:51:48 +0200

    DOM task a done

--- week-three/task1/js/main-a.js
+++ week-three/task1/js/main-a.js
@@ -1,1 +1,17 @@
 // Put code of task A here
+const main = document.querySelector("main");
+
+main.innerHTML += `
+                    <article>
+                        <header>
+                            <h2>Some header</h2>
+                        </header>
+                        <figure>
+                            <img src="http://placekitten.com/320/160" alt="title" />
+                            <figcaption>Caption</figcaption>
+                            </figure>
+                            <p>
+                            Here is some text. Here is some text. Here is some text. Here is some text.
+                            </p>
+                    </article>
+                 `;
