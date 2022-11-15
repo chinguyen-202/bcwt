4a76b3bd136cd1945d89978c431162cbbfe6f03f
Chi Nguyen <chi.nguyen@live.com>
2022-11-15 13:08:03 +0200

    task2 week3 done

--- week-three/task2/script.js
+++ week-three/task2/script.js
@@ -1,19 +1,54 @@
 "use strict";
-(async () => {
-  try {
-    const response = await fetch("pics.json");
-    if (!response.ok) {
-      throw new Error("something went wrong");
-    }
-    const images = await response.json();
-    const n = images[1].name; // name property of the second item of 'images' array
-    const d = images[1].description; // description property of the second item of 'images' array
-    const u = images[1].url; // url property of the second item of 'images' array
 
-    document.querySelector("img").src = u;
-    document.querySelector("img").alt = n;
-    document.querySelector("figcaption").innerHTML = d;
-  } catch (error) {
-    console.log(error);
-  }
-})();
+const baseUrl = "https://api.tvmaze.com";
+
+const form = document.querySelector("#search-form");
+const query = document.querySelector("#query");
+const result = document.querySelector("#result");
+
+const fetchShow = async (url) => {
+  const response = await fetch(url);
+  return await response.json();
+};
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const url = form.action + "?q=" + query.value;
+  const tvShows = await fetchShow(url);
+  displayShows(tvShows);
+  console.log(tvShows);
+});
+
+const displayShows = (tvShows) => {
+  const result = document.querySelector("#result");
+  result.innerHTML = "";
+
+  if (tvShows.length == 0)
+    result.innerHTML += `<article>
+                                            <header>
+                                                <h2 id="name">Can not find movie</h2>
+                                            </header>
+                                        </article>`;
+  else
+    tvShows.forEach((show) => {
+      result.innerHTML += `<article>
+                        <header>
+                            <h2 id="name">${show.show.name}</h2>
+                        </header>
+                        <figure>
+                            <img src="${
+                              show.show.image
+                                ? show.show.image.medium
+                                : "./image_not_available.jpeg"
+                            }" alt="${show.title}">
+                            <figcaption>${
+                              show.show.image ? show.show.name : ""
+                            }</figcaption>
+                        </figure>
+                        <p>${show.show.summary ? show.show.summary : ""}</p>
+                        <a href="${show.show.officialSite}">${
+        show.show.officialSite ? "Official Site" : ""
+      }</a>
+                      </article>`;
+    });
+};
