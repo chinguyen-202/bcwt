4d1e1e1e5667b6c60864294d7703d58db396c2de
Chi Nguyen <chi.nguyen@live.com>
2022-11-22 15:06:59 +0200

    session task done

--- state-server/app.js
+++ state-server/app.js
@@ -1,6 +1,7 @@
 "use strict";
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const app = express();
 const port = 3000;
 
@@ -15,6 +16,14 @@ app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "ghjkdgfADHShjfkads",
+    saveUninitialized: true,
+    resave: true,
+    cookie: { maxAge: 6000 },
+  })
+);
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -25,7 +34,7 @@ app.get("/form", (req, res) => {
 });
 
 app.get("/secret", (req, res) => {
-  if (loggedIn) {
+  if (req.session.loggedIn) {
     res.render("secret");
   } else {
     res.redirect("/form");
@@ -34,13 +43,13 @@ app.get("/secret", (req, res) => {
 
 app.post("/login", (req, res) => {
   // check for usename/password match
-  console.log(req.body);
+  console.log(req.session);
   if (
     req.body.username == user.username &&
     req.body.password == user.password
   ) {
     // set session variable
-    loggedIn = true;
+    req.session.loggedIn = true;
   }
   res.redirect("/secret");
 });
@@ -59,6 +68,11 @@ app.get("/setCookie/:color", (req, res) => {
 app.get("/deleteCookie", (req, res) => {
   res.clearCookie("color");
   res.send("color cookie removed");
+});
+
+app.get("/logout", (req, res) => {
+  req.session.loggedIn = false;
+  res.redirect("/");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
