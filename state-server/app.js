d21723d1e269cf0ae37ae124caae209712b9947a
Chi Nguyen <chi.nguyen@live.com>
2022-11-22 14:26:49 +0200

    task session start

--- state-server/app.js
+++ state-server/app.js
@@ -4,12 +4,45 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const port = 3000;
 
+const user = {
+  username: "foo",
+  password: "bar",
+};
+let loggedIn = false;
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/form", (req, res) => {
+  res.render("form");
+});
+
+app.get("/secret", (req, res) => {
+  if (loggedIn) {
+    res.render("secret");
+  } else {
+    res.redirect("/form");
+  }
+});
+
+app.post("/login", (req, res) => {
+  // check for usename/password match
+  console.log(req.body);
+  if (
+    req.body.username == user.username &&
+    req.body.password == user.password
+  ) {
+    // set session variable
+    loggedIn = true;
+  }
+  res.redirect("/secret");
 });
 
 app.get("/getCookie", (req, res) => {
