c1f68162e1b430a06be211b3c4774584be01293d
Chi Nguyen <chi.nguyen@live.com>
2022-11-25 15:12:25 +0200

    wip:add passport auth

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const app = express();
 const cors = require("cors");
 const catRouter = require("./routes/catRoute");
 const userRouter = require("./routes/userRoute");
+const authRouter = require("./routes/authRoute");
+const passport = require("./utils/passport");
 const port = 3000;
 
 app.use(cors());
@@ -14,7 +16,11 @@ app.use(express.urlencoded({ extended: true })); // for parsing application/x-ww
 //serve upload files
 app.use(express.static("/uploads"));
 
-app.use("/cat", catRouter);
-app.use("/user", userRouter);
+app.use(passport.initialize());
+//app.use(passport.session());
+
+app.use("/cat", passport.authenticate("jwt", { session: false }), catRouter);
+app.use("/user", passport.authenticate("jwt", { session: false }), userRouter);
+app.use("/auth", authRouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
